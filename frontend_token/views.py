61f88906882893e02ed59ac858c67885e2816c11
javierpugliese <javierpugliese2000@gmail.com>
2018-06-11 11:39:02 -0300

    Uncomment indexPreceptor view.

--- frontend_token/views.py
+++ frontend_token/views.py
@@ -1,17 +1,13 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from f2kens.models import *
 # Create your views here.
 
 def index(request):
 	return render(request, 'index.html')
 
-
-#DONE
-"""
 def indexPreceptor(request):
 	context = []
 	return render(request, 'preceptor.html', context)
-"""
