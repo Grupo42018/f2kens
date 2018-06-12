770a21c5f7c41c680ded7d79f0d6d0dd3c65c82a
marianovalle13 <marianovalle13@gmail.com>
2018-06-12 10:53:40 -0300

    documentacion de modelos

--- f2kens/models.py
+++ f2kens/models.py
@@ -7,47 +7,53 @@ from . import apiModel
 import datetime
 # Create your models here.
 
+# Confirmaciones de Tutor
 F2_STATES = [
     ('En Espera'),
     ('Aprovado'),
     ('Rechazado')
 ]
 
+#clase preceptor usuario
 class Preceptor(models.Model):
     api_id=models.IntegerField()
     user=models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
+#clase tutor usuario
 class Parent(models.Model):
     api_id = models.IntegerField()
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
+#clase dispositivo de tutor  
 class Device(models.Model):
     token = models.CharField(max_length=128)
     parent = models.ForeignKey(Parent, on_delete=models.CASCADE)
 
+#clase formulario
 class Formulario(models.Model):
     student = models.IntegerField()
     date = models.DateField(auto_now=True)
     time = models.TimeField()
     preceptor = models.ForeignKey(Preceptor)
 
-    class Meta:
+    class Meta:         ###clase que sirve para cambiar la forma en la que se muestra (Formulario/Formularios)
         abstract=True
         verbose_name='Formulario'
         verbose_name_plural='Formularios'
 
-class Formulario2(Formulario):
+class Formulario2(Formulario):      ###clase formulario 2
     motivo_docente = models.CharField(max_lenght = 300)
     
-class Formulario3(Formulario):
+class Formulario3(Formulario):      ###clase formulario 3
     motivo_alumno = models.CharField(max_lenght = 300)
 
-
+#clase curso y año 
 class ApiYear(apiModel.ApiModel):
     _url = 'years/'
     year_number = apiModel.Field(int)
     division = apiModel.Field(str)
 
+#clase api.preceptor 
 class ApiPreceptor(apiModel.ApiModel):
     _url = 'preceptors/'
     first_name = apiModel.Field(str)
@@ -56,6 +62,7 @@ class ApiPreceptor(apiModel.ApiModel):
     email = apiModel.Field(str)
     internal_tel = apiModel.Field(int)
 
+#clase api.Estudiante 
 class ApiStudent(apiModel.ApiModel):
     _url = 'students/'
     first_name = apiModel.Field(str)
@@ -65,18 +72,24 @@ class ApiStudent(apiModel.ApiModel):
     status = apiModel.Field(int)
     year = apiModel.Field(ApiYear)
 
+
+#clase api.Tutor 
 class ApiParent(apiModel.ApiModel):
     _url = 'parents/'
     first_name = apiModel.Field(str)
     last_name = apiModel.Field(str)
     email = apiModel.Field(str)
 
+
+#clase api.Registro
 class ApiRegistro(apiModel.ApiModel):
     _url = 'registros/'
     year = apiModel.Field(ApiYear)
     preceptor = apiModel.Field(ApiPreceptor)
     date = apiModel.Field(datetime.datetime.strptime, False, "%Y-%m-%d")
 
+
+#clase api.Ausencia
 class ApiAbsence(apiModel.ApiModelSaveable):
     _url = 'absence/'
     origin = apiModel.Field(int)
