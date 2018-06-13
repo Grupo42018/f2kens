1de16e9e668bd742712861a3ac9866ea20409b87
javierpugliese <javierpugliese2000@gmail.com>
2018-06-13 12:09:29 -0300

    Added a getter-object view for f2.

--- frontend_token/views.py
+++ frontend_token/views.py
@@ -14,3 +14,8 @@ def indexPreceptor(request):
 	context['preceptores'] = Preceptor.objects.all()
 	context['students'] = ApiStudent.getAll()
 	return render(request, 'preceptor.html', context)
+
+def getForms2(request):
+	context = {}
+	context['formularios2'] = Formulario2.objects.all()
+	return render(request, 'stateF2.html', context)
