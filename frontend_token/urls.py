986d7610580ed5eaa62d82ca35e5ee37a75f17f5
mateosenes <mateosenes@gmail.com>
2018-06-21 11:08:27 -0300

    Added auth urls.

--- frontend_token/urls.py
+++ frontend_token/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from django.contrib import admin
 from frontend_token.views import *
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('', index, name='frontend_index'),
+    path('', auth_views.login, {'template_name': 'login.html'}, name='login'),
+    path('bye/', auth_views.logout, name='logout'),
     path('main_preceptor/', index_preceptor, name='index_preceptor'),
     path('get_f2/', get_forms2, name='get_f2')
 ]
