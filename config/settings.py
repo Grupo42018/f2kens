a98637c6922be5067e03754a612cada7a843181c
javierpugliese <javierpugliese2000@gmail.com>
2018-06-22 10:58:02 -0300

    Changed login redirect to check_user view.

--- config/settings.py
+++ config/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Email sending config stuff
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'f2kens@gmail.com' 
@@ -24,20 +23,19 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
 # Login config stuff
-LOGIN_REDIRECT_URL = 'admin:index'
+LOGIN_REDIRECT_URL = 'check_user'
 LOGOUT_REDIRECT_URL = 'login'
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'g$ml73umt#o%by-tad-*_w&18$pm=(8s3$5_ruaod45!gd^=(u'
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
-
 
 # Application definition
 
