5cf9bc8cb411ccce00333f99e269ec3b21f8b96a
sojasan <emilioagg@hotmail.com>
2018-11-27 08:25:13 -0300

    added URL create_parent

--- f2kens/urls.py
+++ f2kens/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('create_f2/', create_f2, name='create_f2'),
     path('update_f2_state/form_id_<int:form2_id>/', update_f2_state, name='estado_f2'),
     path('get_f2s/', get_f2s, name="get_f2s"),
-    path('get_years/', get_years, name="get_years")
+    path('get_years/', get_years, name="get_years"),
+    path('create_parent/',create_parent, name='create_parent')
     ]
